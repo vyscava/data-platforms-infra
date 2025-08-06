d1682749a6f7d09211ac93ce9b51ecccf2a53110
Vitor B Garbim <vyscava@gmail.com>
2025-08-05 20:38:25 -0500

    Latests updates on Spark

--- spark/scripts/run.sh
+++ spark/scripts/run.sh
@@ -4,21 +4,6 @@ set -e
 
 export SPARK_NO_DAEMONIZE=true
 
-# Start Spark
-case "$SPARK_MODE" in
-    master)
-        echo "Starting Spark Master..."
-        exec start-master.sh
-        ;;
-    worker)
-        echo "Starting Spark Worker..."
-        exec start-worker.sh
-        ;;
-    *)
-        echo "SPARK_MODE is not set to 'master' or 'worker'. Skipping Spark startup."
-        ;;
-esac
-
 # Start JupyterLab if requested
 if [[ "$JUPYTER_MODE" == "master" ]]; then
     # echo "Building JupyterLab..."
@@ -34,6 +19,21 @@ if [[ "$JUPYTER_MODE" == "master" ]]; then
         --no-browser \
         --allow-root &
 fi
+
+# Start Spark
+case "$SPARK_MODE" in
+    master)
+        echo "Starting Spark Master..."
+        exec start-master.sh
+        ;;
+    worker)
+        echo "Starting Spark Worker..."
+        exec start-worker.sh $SPARK_MASTER_URL
+        ;;
+    *)
+        echo "SPARK_MODE is not set to 'master' or 'worker'. Skipping Spark startup."
+        ;;
+esac
 
 # Wait for backgrounded processes (like JupyterLab)
 # wait
